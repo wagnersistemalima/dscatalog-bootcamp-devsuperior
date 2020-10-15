3429d8d86137a803b02adee00c5db1c3b27718da
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-10-15 11:15:28 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar';
 
 const App = () => {
     
     return (
-        <h1>App</h1>
+        <Navbar />
     );
 }
 
