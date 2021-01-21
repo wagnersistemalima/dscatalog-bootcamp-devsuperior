d050244b764873057922207f0dedefc317ebc0b0
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-01-21 12:15:35 -0300

    add admin navbar

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const Admin = () => {
     return (
         <div>
-            <h1>Admin</h1>
+            <Navbar />
         </div>
     );
 }
