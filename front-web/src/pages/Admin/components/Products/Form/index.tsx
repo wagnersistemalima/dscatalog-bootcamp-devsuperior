a8b4f4922c4eaacdcd840071ac32211c3c12a16e
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-02-08 11:57:48 -0300

    Finalizando parte 3 front-end

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,22 +1,114 @@
-import React from 'react';
+import { makeRequest } from 'core/utilis/request';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
+// estrutura de formulario
+
+type FormState = {
+    name: string;
+    price: string;
+    category: string;
+    description: string;
+}
+
+type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+
+
+
 const Form = () => {
+
+    const [formData, setFormData] = useState<FormState>({
+        name : '',
+        price : '',
+        category : '1',
+        description: ''
+    });
+    
+
+    const handleOnChange = (event: FormEvent) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData(data => ({...data, [name] : value }));
+
+    }
+
+    
+
+    // cadastrar recurso
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const payload = {
+            ...formData,
+            imgUrl : 'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202009/17/00194481000027____2__640x640.jpg',
+            categories: [{id : formData.category}]
+        }
+        makeRequest({url: '/products', method: 'POST', data: payload})
+        .then(() => {
+            setFormData({name: '', category: '', price: '', description: ''})
+        })
+        
+    }
+
     return (
-        <BaseForm title= "cadastrar um produto">
-            <div className="row">
-                <div className="col-6">
-                    <input type = "text" className="form-control" />
+        <form onSubmit = {handleSubmit}>
+            <BaseForm title= "cadastrar um produto">           
+            
+                <div className="row">
+                    <div className="col-6">
+                        <input
+                            value = {formData.name}
+                            name = "name" 
+                            type = "text" 
+                            className = "form-control mb-5" 
+                            onChange = {handleOnChange}
+                            placeholder = "Nome do produto"
+                        />
+                        <select 
+                            value = {formData.category}
+                            className="form-control mb-5" 
+                            onChange = {handleOnChange}
+                            name = "category"
+                            
+                        >
+                        
+                            <option value="1">Livros</option>
+                            <option value="2">Eletrônicos</option>
+                            <option value="3">Computadores</option>
+                        
+                        </select>
+
+                        <input
+                            value = {formData.price}
+                            name = "price" 
+                            type = "text" 
+                            className = "form-control" 
+                            onChange = {handleOnChange}
+                            placeholder = "Preço"
+                        />
+
+                    </div>
+                    <div className="col-6">
+                        <textarea 
+                            name="description"
+                            value = {formData.description}
+                            onChange = {handleOnChange}
+                            className="form-control" 
+                            cols={30}
+                            rows={10} 
+                        
+                        
+                        />
+
+                    </div>
 
                 </div>
 
-            </div>
 
-
-        </BaseForm>
+            </BaseForm>
             
-            
+        </form>    
             
         
     );
